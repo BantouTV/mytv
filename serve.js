43e91bb47f635f85ee37674b83f11655defb0eeb
Thomas Bassetto <tbassetto@gmail.com>
2011-07-08 15:20:49 +0200

    Enable CORS on server.js

--- serve.js
+++ serve.js
@@ -9,64 +9,59 @@
  * Date: Wed Jun 29 16:25:37 2011
  */
 
-
-Joshfire = {define: function(a,b) {_ = b();}};
-  
+Joshfire = {define: function(a, b) {_ = b();}};
 
 var express = require('express');
 var fs = require('fs');
-var expressApp = express.createServer(),
- request = require('request');
+var expressApp = express.createServer();
+var request = require('request');
 
-
-
-
-expressApp.configure(function(){
+expressApp.configure(function() {
 //    app.set('views', __dirname + '/views');
-    
-    expressApp.use(express.logger({ format : ":method :url"}));
-    expressApp.use(express.bodyParser());
-    
-    expressApp.use(expressApp.router);
-    
-    var testPath = __dirname + '/';
-    expressApp.use(express.static(testPath));
-    
-  
+
+  expressApp.use(express.logger({ format : ":method :url"}));
+  expressApp.use(express.bodyParser());
+
+  expressApp.use(expressApp.router);
+
+  var testPath = __dirname + '/';
+  expressApp.use(express.static(testPath));
 });
 
 var serialize = function(obj) {
   var str = [];
-  for(var p in obj)
-     str.push(p + "=" + encodeURIComponent(obj[p]));
+  for(var p in obj) {
+   str.push(p + "=" + encodeURIComponent(obj[p]));
+  }
   return str.join("&");
 };
 
+// expressApp.get('/proxy.php', function(req, res) {
+//   res.end('Coucou');
+// });
 
-
-expressApp.get('/proxy.php', function(req, res){
-  res.end('Coucou');
-})
-
-expressApp.post('/proxy/', function(req, res){
-    
-    var rq = {'uri':req.body.url,'method':'POST'};
-    console.log('requete post via proxy', req.param('url'), req.body);
-    if (req.body!==undefined) {
-        rq["json"] = req.body.data;
-        rq["headers"] = {"Content-Type":"application/x-www-form-urlencoded"};
-    } else {
-        rq["headers"] = {"Content-Length":"0"};
-    }
-    
-    console.log('proxy call', rq);
-    
-    request(rq,function(error, response, body) {
-
-        res.send(body);
-    });
- 
+// Allow CORS
+expressApp.options('/proxy/', function(req, res) {
+  res.send('', { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'X-Requested-With' }, 200);
 });
-expressApp.listen(40010);
 
+expressApp.all('/proxy/', function(req, res) {
+  // console.warn('received request');
+  var rq = {'uri': req.body.url, 'method': 'POST'};
+
+  // console.log('requete post via proxy', req.param('url'), req.body);
+  if (req.body !== undefined) {
+    rq['json'] = req.body.data;
+    rq['headers'] = {'Content-Type': 'application/x-www-form-urlencoded'};
+  } else {
+    rq['headers'] = {'Content-Length': '0'};
+  }
+  
+  // console.log('proxy call', rq);
+  request(rq, function(error, response, body) {
+    res.send(body, { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'X-Requested-With' });
+  });
+});
+
+expressApp.listen(40010);
 console.log('Listening on 40010.')
