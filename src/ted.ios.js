04df4450d19c130aa7e8692c98a1022f3af06550
Thomas Bassetto <tbassetto@gmail.com>
2011-07-08 15:23:30 +0200

    Consolidate userSession object

--- src/ted.ios.js
+++ src/ted.ios.js
@@ -13,89 +13,99 @@ Joshfire.define(['./app', 'joshfire/class', 'joshfire/vendor/underscore', './jos
   return Class(App, {
 
     id: 'myTED',
-  
+
     setup: function(callback) {
       var self = this;
 
       this.__super(function() {
 
         var likeButton = self.ui.element('/main/home/videodetail/like');
+        
         likeButton.subscribe('input', function(ev, id) {
-          
+
           $('#' + likeButton.htmlId).toggleClass('liked');
+
           if (!self.userSession){
             //Should be connected. Prompt ?
             return false;
           }
-          var favorites = _.extend([],self.userSession.mytv.favorites),
-            videoid =  ''+self.ui.element('/main/home/videodetail').dataPath.match(/[0-9]+$/)[0];
-          if (_.include(favorites, videoid)){
+
+          // self.userSession.mytv.favorites could not exist yet FIXME
+          if (!self.userSession.mytv.favorites) {
+            self.userSession.mytv.favorites = [];
+          }
+
+          var favorites = _.extend([], self.userSession.mytv.favorites),
+              videoid = '' + self.ui.element('/main/home/videodetail').dataPath.match(/[0-9]+$/)[0];
+
+          if (_.include(favorites, videoid)) {
             //unfavorite
             favorites = _.without(favorites, videoid);
-          }
-          else{
+          } else{
             //favorite
             favorites.push(videoid);
           }
-            JoshmeAPI.setData(self,self.userSession.uid, {favorites:favorites}, function (err, retour){
-              self.userSession.mytv.favorites=favorites;
-          });
-          
-        });
 
-       
+          JoshmeAPI.setData(self, self.userSession.uid, {favorites: favorites}, function (err, retour) {
+            self.userSession.mytv.favorites = favorites;
+          });
+        });
 
         self.fbInit(callback);
       });
     },
-  
+
     fbInit: function(callback) {
       var self = this;
-      
+
       window.fbAsyncInit = function() {
         FB.init({appId: 214358631942957, status: true, cookie: true, xfbml: true});
-        
-        //TODO on iPad this is not fired sometimes.
+
+        // TODO on iPad this is not fired sometimes.
         FB.getLoginStatus(function(response) {
           var loginButton = self.ui.element('/toolbar/loginButton').htmlEl;
 
           if (response.session) {
             // logged in and connected user, someone you know
             self.userSession = response.session;
-            self.setState("auth",true);
-            loginButton.innerHTML='Logout';
-            loginButton.onclick= self.fbLogout;
-            
+            self.setState('auth', true);
+            loginButton.innerHTML = 'Logout';
+            loginButton.onclick = self.fbLogout;
+
+            console.warn('self.userSession', self.userSession);
+
             /* Get myTV data */
-            
-            JoshmeAPI.getData(self, self.userSession.uid, function (err, retour){
-                if (!err && retour){
-                  self.userSession.mytv = retour;
-                }
+            JoshmeAPI.getData(self, self.userSession.uid, function (err, data) {
+              console.warn("getData(self …", err, data);
+              if (!err && data) {
+                self.userSession.mytv = data;
+              } else {
+                self.userSession.mytv = {};
+              }
+              console.warn("self.userSession", self.userSession);
             });
-            
-            
           } else {
             // no user session available, someone you dont know
-            self.setState("auth",false);
-            loginButton.innerHTML='<a href="http://www.facebook.com/dialog/oauth?client_id=214358631942957&redirect_uri='+window.location+'&display=touch&scope=read_stream,publish_stream,offline_access">Login</a>';
+            self.setState('auth', false);
+            loginButton.innerHTML = '<a href="http://www.facebook.com/dialog/oauth?client_id=214358631942957&redirect_uri=' + window.location + '&display=touch&scope=read_stream,publish_stream,offline_access">Login</a>';
           }
         });
       };
-      
-      var e = document.createElement('script'); e.async = true;
-      e.src = "http://connect.facebook.net/en_US/all.js"; // + (Joshfire.debug?'//static.ak.fbcdn.net/connect/en_US/core.debug.js':'//connect.facebook.net/en_US/all.js');
+
+      var e = document.createElement('script');
+      e.async = true;
+      e.src = 'http://connect.facebook.net/en_US/all.js'; // + (Joshfire.debug?'//static.ak.fbcdn.net/connect/en_US/core.debug.js':'//connect.facebook.net/en_US/all.js');
       document.getElementById('fb-root').appendChild(e);
-      
+
       callback();
     },
-  
+
     fbLogin:function() {
       var self = this;
       
       if (!FB) return;
       FB.login(function(response) {
-                if (response.session) {
+        if (response.session) {
           if (response.perms) {
             
             // user is logged in and granted some permissions.
@@ -110,10 +120,10 @@ Joshfire.define(['./app', 'joshfire/class', 'joshfire/vendor/underscore', './jos
         }
       }, {perms:'read_stream,publish_stream,offline_access'});
     },
-    fbLogout:function(){
-       FB.logout(function() {
-          window.location = window.location;
-        });
+    fbLogout: function() {
+      FB.logout(function() {
+        window.location = window.location;
+      });
     }
   });
 });
